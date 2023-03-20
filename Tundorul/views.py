fc2d0faa0d16a9cccbb72b737d559831a401e871
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-03-20 17:57:25 +0000

    installed all dependencies on a venv in the correct way, and created the first view for the home page on start

--- Tundorul/views.py
+++ Tundorul/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+        template_name = 'index.html'
+        return render(
+            request,
+            'index.html',
+            {}
+        )
+        return HttpResponse('')
