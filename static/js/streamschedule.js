6bbda024062112a8c057b0ae0c0d0c8af992a607
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-05-08 21:37:27 +0000

    created the ability to include 2 streams in a day for the countdown to work

--- static/js/streamschedule.js
+++ static/js/streamschedule.js
@@ -1,69 +1,59 @@
 class LiveCountDown{
     constructor(){
-
-        this.callendar = {}
-        for (let [day, time] of Object.entries(dailyHours)){
-            this.callendar[day] = {
-                h: time.split(':')[0],
-                m: time.split(':')[1],
-                secStamp: time.split(':')[0]*3600 + time.split(':')[1]* 60
-            }
-        }
-        for (let i = 0; i < 7; i++){
-           if(!this.callendar[i]) this.callendar[i] = 'restDay'
-        }
-
+        this.nextStreamStamp = 0
+        this.importedTimes = dailyHours
+        this.calendar = []
         this.now = Date.now()
-        this.currentWeekDay = new Date(this.now).getDay() // integer
-
-
-        this.leftOfDaySecStamp = 0
-        this.nowH = new Date(this.now).getHours()
-        this.nowMin = new Date(this.now).getMinutes()
-        this.nowSec = new Date(this.now).getSeconds()
-        this.nowSecStamp = this.nowSec + this.nowMin * 60 + this.nowH * 3600
-
+        let currentWeekDay = new Date(this.now).getDay() // integer
+        let nowH = new Date(this.now).getHours()
+        let nowMin = new Date(this.now).getMinutes()
+        let nowSec = new Date(this.now).getSeconds()
+        this.weekNowSecStamp = currentWeekDay * 24 * 3600 + (nowH * 3600 + nowMin * 60 + nowSec)
         this.clockDiv = document.querySelector('.schedule-timer') // timer container
     }
 
+    buildCalendarStamps(){
+
+        for (let segment of this.importedTimes){
+            let stamp =  segment.day * 24 * 3600 + segment.time.split(':')[0] * 3600 + segment.time.split(':')[1] * 60
+            this.calendar.push(stamp)
+        }
+    }
+
+    setNextStream(){
+        for (let segment of this.calendar){
+            if (segment > this.weekNowSecStamp){
+                this.nextStreamStamp = segment - this.weekNowSecStamp
+                break
+            }
+        }
+         if (this.weekNowSecStamp >= this.calendar[this.calendar.length-1]  ){
+            this.nextStreamStamp = this.calendar[0] + 24*7*3600 - this.weekNowSecStamp
+        }
+
+    }
 
     renderClock(){
-        let totalSecStamp =  this.leftOfDaySecStamp
-        let seconds = totalSecStamp%60 % 60
-        let minutes = Math.floor(totalSecStamp/60 % 60)
-        let hours = Math.floor(totalSecStamp/60/60)
-
+        let stamp = this.nextStreamStamp
+        let seconds = stamp%60%60
+        let minutes = Math.ceil(stamp/60%60)
+        let hours = Math.floor(stamp/60/60)
         let localeH = hours.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
         let localeM = minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
         let localeS = seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
         this.clockDiv.textContent = `${localeH}: ${localeM}: ${localeS}`
     }
-
-    findNextStream() {
-        let searchDay = this.currentWeekDay
-        for (let i = 0; i < 7; i++) {
-            let calSearch = this.callendar[searchDay]
-            if (calSearch == 'restDay' || this.nowSecStamp > calSearch.secStamp) {
-                searchDay = searchDay+1>6? 0 : searchDay+1
-                this.leftOfDaySecStamp += 24*3600 - this.nowH*3600 - this.nowMin * 60 - this.nowSec
-            }else{
-                this.streamStartHour = calSearch.h
-                this.streamStartMin = calSearch.m
-                this.leftOfDaySecStamp += calSearch.secStamp - this.nowSecStamp
-                break
-            }
-        }
-    }
 }
-
 function main(){
     let countdownState = new LiveCountDown()
-    countdownState.findNextStream()
+    countdownState.buildCalendarStamps()
+    countdownState.setNextStream()
     countdownState.renderClock()
     setTimeout(main, 1000)
 }
 
 main()
+
 
 
 
