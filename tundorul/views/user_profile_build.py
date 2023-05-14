db76a609e03955ef2b196d757e1db1b814e0e098
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-05-14 18:23:15 +0000

    user_profile_build.py saves the user following status in the model

--- tundorul/views/user_profile_build.py
+++ tundorul/views/user_profile_build.py
@@ -9,6 +9,26 @@ from allauth.socialaccount.models import SocialAccount, SocialToken
 import requests
 from django.shortcuts import render, get_object_or_404, reverse
 import os
+import json
+
+def is_follower(extra_data, token):
+    headers = {
+        'Authorization': 'Bearer ' + f'{token}',
+        'Client-Id': os.environ.get('CLIENT_ID'),
+    }
+
+    params = {
+        'user_id': extra_data['id'],
+    }
+    url = 'https://api.twitch.tv/helix/channels/followed'
+    following_streams_request = requests.get(url, headers=headers, params=params)
+    following_streams = json.loads(following_streams_request.content.decode("utf-8"))['data']
+
+    if following_streams:
+        for followed in following_streams:
+            if followed['broadcaster_login'] == 'tundorul':
+                return True
+
 
 @receiver(user_logged_in)
 def update_user_profile(request, user, **kwargs):
@@ -16,41 +36,31 @@ def update_user_profile(request, user, **kwargs):
     sociallogin = get_object_or_404(query_set_social)
     query_set_token = SocialToken.objects.filter(account=sociallogin)
     token = get_object_or_404(query_set_token).token
+    extra_data = sociallogin.extra_data
+
+    is_following = is_follower(extra_data, token)
 
     if UserProfile.objects.filter(username=sociallogin.user).exists():
-        extra_data = sociallogin.account.extra_data
-        token = sociallogin.token
 
-        headers = {
-            'Authorization': 'Bearer ' + f'{token}',
-            'Client-Id': os.environ.get('CLIENT_ID'),
-        }
-
-        params = {
-            'user_id': extra_data['id'],
-        }
-
-        url = 'https://api.twitch.tv/helix/streams/followed'
-        followingStreams = requests.get(url, headers=headers, params=params)
-        print(followingStreams)
         instance = get_object_or_404(UserProfile.objects.filter(username=sociallogin.user))
         instance.username = sociallogin.user
         instance.uid = extra_data['id']
         instance.current_name = extra_data['display_name']
         instance.email = extra_data['email']
-        # is_follower = request
+        instance.is_follower = is_following
         # is_subscribed = request
         instance.save()
 
     else:
-        print(sociallogin)
-        extra_data = sociallogin.account.extra_data
-        token = sociallogin.token
         print('added')
         user_profile = UserProfile.objects.create(
             username=sociallogin.user,
             uid=extra_data['id'],
             current_name=extra_data['display_name'],
             email=extra_data['email'],
+            is_follower=is_following
         )
         user_profile.save()
+
+
+
