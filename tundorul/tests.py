5ad942022a3e2ed4c2a035b9789965a5ac75d217
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-05-29 20:17:43 +0000

    3 tests done get_home_page, get_vods_page, refresh_vods

--- tundorul/tests.py
+++ tundorul/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from tundorul.models import Vods, UserProfile
+import datetime
 
 
 class TestViews(TestCase):
@@ -8,14 +10,36 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_add_suggestions(self):
-        pass
-
     def test_refresh_vods(self):
-        pass
+        Vods.objects.create(
+            id=1822284878,
+            title='[Part 32] GoT - starting the DLC on Iki Island',
+            url='https://www.twitch.tv/videos/1822284878',
+            thumbnail_url='https://static-cdn.jtvnw.net/cf_vods/dgeft87w_168idth}x%{height}.jpg',
+            published_at=datetime.datetime(2023, 5, 17, 26, 30, 11, tzinfo=None),
+            view_count=1576,
+            stream_id=12937432,
+        )
+        Vods.objects.all().delete()
+        vod = Vods.objects.filter(title='[Part 32] GoT - starting the DLC on Iki Island')
+        self.assertEqual(len(vod), 0)
+        Vods.objects.create(
+            id=123432141,
+            title='Test Vod Is Here',
+            url='https://www.twitch.tv/videos/1822284578',
+            thumbnail_url='https://static-cdn.jtvnw.net/cf_vo4341006//thumb/thumb0-%{width}x%{height}.jpg',
+            published_at=datetime.datetime(2023, 5, 17, 16, 30, 11, tzinfo=None),
+            view_count=999,
+            stream_id=1822284878,
+        )
+        vod = Vods.objects.filter(title='Test Vod Is Here')
+        self.assertEqual(len(vod), 1)
+
 
     def test_get_vods_page(self):
-        pass
+        response = self.client.get('/vods')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'vods.html')
 
     def test_get_vods(self):
         pass
